2e1a9544f106d9624dddf2bac4f0d608b1b17f44
Harshal39 <harshalbhute39@gmail.com>
2023-02-23 16:15:41 +0530

    Initial commit

---
+++ Game.cpp
@@ -0,0 +1,229 @@
+#include<iostream>// provides input and output operations through the console or other input/output streams
+#include<conio.h>//performing console input/output operations 
+#include<direct.h>// provides functions for manipulating directories and their contents
+#include<windows.h>// provides functions and constants for interacting with the Windows operating system
+#include<time.h>//provides functions for working with date and time
+
+#define SCREEN_WIDTH 90 //width of a console or graphical user interface (GUI) screen
+#define SCREEN_HEIGHT 26 //Height of a console or graphical user interface (GUI) screen
+#define WIN_WIDTH 70 //represent the width of a window or interface element in the program
+
+using namespace std; //allows to use names from the standard library 
+HANDLE console=GetStdHandle(STD_OUTPUT_HANDLE); //retrieves a handle to the console output buffer
+COORD CursorPosition;//COORD is a Windows API data type that represents a two-dimensional coordinate
+//program creates a container for storing the current position of the cursor on the console window
+
+int enemyY[3];//array of integers with the identifier enemyY and a length of 3 used to store the vertical positions of enemies in a game or simulation
+int enemyX[3];//array of integers with the identifier enemyX and a length of 3 used to store the horizontal positions of enemies in a game or simulation
+int enemyFlag[3];//array of integers with the identifier enemyFlag and length of 3 used to store flags or status indicators associated with each enemy in a game or simulation
+char car[4][4] = {
+    {' ', '+', '+', ' '},
+    {'+', '+', '+', '+'},
+    {'+', '+', '+', '+'},
+    {'+', '+', '+', '+'}
+};//char car[4][4] declares a two-dimensional array of characters with the identifier car and dimensions 4 x 4  used in a game or simulation that involves moving cars
+
+int carPos = WIN_WIDTH/2;//calculates the horizontal position of the center of the game window, used to store the current position of the player's car in a game or simulation
+int score = 0; //initail score of gamer will be zero
+
+void gotoxy(int x, int y)////gotoxy moves the cursor to the specified position on the console screen used from header conio.h,uses Windows API functions to move the cursor to the specified position on the console screen
+{
+	CursorPosition.X = x;
+	CursorPosition.Y = y;
+	SetConsoleCursorPosition(console, CursorPosition);
+}//This function moves the cursor to the specified position on the console screen, which can be used to print text or other graphics at that location.
+void setcursor(bool visible, DWORD size)
+ {
+	if(size == 0)
+		size = 20;	
+	
+	CONSOLE_CURSOR_INFO lpCursor;	
+	lpCursor.bVisible = visible;
+	lpCursor.dwSize = size;
+	SetConsoleCursorInfo(console,&lpCursor);
+}//this function is used to change the visibility and size of the console cursor in a console-based application or game
+void drawBorder(){  
+	for(int i=0; i<SCREEN_HEIGHT; i++){
+		for(int j=0; j<17; j++){
+			gotoxy(0+j,i); cout<<"±";
+			gotoxy(WIN_WIDTH-j,i); cout<<"±";
+		}
+	} 
+	for(int i=0; i<SCREEN_HEIGHT; i++){
+		gotoxy(SCREEN_WIDTH,i); cout<<"±";
+	} 
+}
+void genEnemy(int ind){
+	enemyX[ind] = 17 + rand()%(33);  
+}
+void drawEnemy(int ind){
+	if( enemyFlag[ind] == true ){
+		gotoxy(enemyX[ind], enemyY[ind]);   cout<<"****";  
+		gotoxy(enemyX[ind], enemyY[ind]+1); cout<<" ** "; 
+		gotoxy(enemyX[ind], enemyY[ind]+2); cout<<"****"; 
+		gotoxy(enemyX[ind], enemyY[ind]+3); cout<<" ** ";  
+	} 
+}
+void eraseEnemy(int ind){
+	if( enemyFlag[ind] == true ){
+		gotoxy(enemyX[ind], enemyY[ind]); cout<<"    ";  
+		gotoxy(enemyX[ind], enemyY[ind]+1); cout<<"    "; 
+		gotoxy(enemyX[ind], enemyY[ind]+2); cout<<"    "; 
+		gotoxy(enemyX[ind], enemyY[ind]+3); cout<<"    "; 
+	} 
+}
+void resetEnemy(int ind){
+	eraseEnemy(ind);
+	enemyY[ind] = 1;
+	genEnemy(ind);
+}
+
+void drawCar(){
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			gotoxy(j+carPos, i+22); cout<<car[i][j];
+		}
+	}
+}
+void eraseCar(){
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			gotoxy(j+carPos, i+22); cout<<" ";
+		}
+	}
+}
+ 
+int collision(){
+	if( enemyY[0]+4 >= 23 ){
+		if( enemyX[0] + 4 - carPos >= 0 && enemyX[0] + 4 - carPos < 9  ){
+			return 1;
+		}
+	}
+	return 0;
+} 
+void gameover(){
+	system("cls");
+	cout<<endl;
+	cout<<"\t\t--------------------------"<<endl;
+	cout<<"\t\t-------- Game Over -------"<<endl;
+	cout<<"\t\t--------------------------"<<endl<<endl;
+	cout<<"\t\tPress any key to go back to menu.";
+	getch();
+}
+void updateScore(){
+	gotoxy(WIN_WIDTH + 7, 5);cout<<"Score: "<<score<<endl;
+}
+
+void instructions(){
+	
+	system("cls");
+	cout<<"Instructions";
+	cout<<"\n----------------";
+	cout<<"\n Avoid Cars by moving left or right. ";
+	cout<<"\n\n Press 'a' to move left";
+	cout<<"\n Press 'd' to move right";
+	cout<<"\n Press 'escape' to exit";
+	cout<<"\n\nPress any key to go back to menu";
+	getch();
+}
+
+void play(){
+	carPos = -1 + WIN_WIDTH/2;
+	score = 0;
+	enemyFlag[0] = 1;
+	enemyFlag[1] = 0;
+	enemyY[0] = enemyY[1] = 1;
+	  
+	system("cls"); 
+	drawBorder(); 
+	updateScore();
+	genEnemy(0);
+	genEnemy(1);
+	
+	gotoxy(WIN_WIDTH + 7, 2);cout<<"Car Game";
+	gotoxy(WIN_WIDTH + 6, 4);cout<<"----------";
+	gotoxy(WIN_WIDTH + 6, 6);cout<<"----------";
+	gotoxy(WIN_WIDTH + 7, 12);cout<<"Control ";
+	gotoxy(WIN_WIDTH + 7, 13);cout<<"-------- ";
+	gotoxy(WIN_WIDTH + 2, 14);cout<<" A Key - Left";
+	gotoxy(WIN_WIDTH + 2, 15);cout<<" D Key - Right"; 
+	
+	gotoxy(18, 5);cout<<"Press any key to start";
+	getch();
+	gotoxy(18, 5);cout<<"                      ";
+	
+	while(1){
+		if(kbhit()){
+			char ch = getch();
+			if( ch=='a' || ch=='A' ){
+				if( carPos > 18 )
+					carPos -= 4;
+			}
+			if( ch=='d' || ch=='D' ){
+				if( carPos < 50 )
+					carPos += 4;
+			} 
+			if(ch==27){
+				break;
+			}
+		} 
+		
+		drawCar(); 
+		drawEnemy(0); 
+		drawEnemy(1); 
+		if( collision() == 1  ){
+			gameover();
+			return;
+		} 
+		Sleep(50);
+		eraseCar();
+		eraseEnemy(0);
+		eraseEnemy(1);   
+		
+		if( enemyY[0] == 10 )
+			if( enemyFlag[1] == 0 )
+				enemyFlag[1] = 1;
+		
+		if( enemyFlag[0] == 1 )
+			enemyY[0] += 1;
+		
+		if( enemyFlag[1] == 1 )
+			enemyY[1] += 1;
+		 
+		if( enemyY[0] > SCREEN_HEIGHT-4 ){
+			resetEnemy(0);
+			score++;
+			updateScore();
+		}
+		if( enemyY[1] > SCREEN_HEIGHT-4 ){
+			resetEnemy(1);
+			score++;
+			updateScore();
+		}
+	}
+}
+
+int main()
+{
+	setcursor(0,0); 
+	srand( (unsigned)time(NULL)); 
+	 
+	do{
+		system("cls");
+		gotoxy(10,5); cout<<" -------------------------- "; 
+		gotoxy(10,6); cout<<" |        Car Game        | "; 
+		gotoxy(10,7); cout<<" --------------------------";
+		gotoxy(10,9); cout<<"1. Start Game";
+		gotoxy(10,10); cout<<"2. Instructions";	 
+		gotoxy(10,11); cout<<"3. Quit";
+		gotoxy(10,13); cout<<"Select option: ";
+		char op = getche();
+		
+		if( op=='1') play();
+		else if( op=='2') instructions();
+		else if( op=='3') exit(0);
+		
+	}while(1);
+	
+	return 0;
+}
